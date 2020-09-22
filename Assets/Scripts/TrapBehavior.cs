56c73f846f0f3a52368907f96cd25be4e731f65e
Darcy <darcelisgutierrez@gmail.com>
2020-09-21 23:47:22 -0400

    Phase 5
    Movement added to cup lid, bounce goes into both cups

---
+++ Assets/Scripts/TrapBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBehavior : MonoBehaviour
+{
+    public Vector3 horizontal;
+    public Vector3 vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-horizontal); 
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(horizontal);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(vertical);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-vertical);
+        }
+    }
+}
